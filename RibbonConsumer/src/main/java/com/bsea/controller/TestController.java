a5e76e19e25b6787313ed4d24c2f4d17e453832c
bseayin <yinyouhai@aliyun.com>
2019-04-06 18:39:50 +0800

    bsea

---
+++ RibbonConsumer/src/main/java/com/bsea/controller/TestController.java
@@ -0,0 +1,59 @@
+package com.bsea.controller;
+
+
+import com.bsea.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("user/{id:\\d+}")
+    public User getUser(@PathVariable Long id) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        URI uri = UriComponentsBuilder.fromUriString("http://Server-Provider/user/{id}")
+                .build().expand(params).encode().toUri();
+        return this.restTemplate.getForEntity(uri, User.class).getBody();
+    }
+
+    @GetMapping("user")
+    public List<User> getUsers() {
+        return this.restTemplate.getForObject("http://SERVER-PROVIDER/user", List.class);
+    }
+
+    @GetMapping("user/add")
+    public String addUser() {
+        User user = new User(1L, "mrbird", "123456");
+        HttpStatus status = this.restTemplate.postForEntity("http://SERVER-PROVIDER/user", user, null).getStatusCode();
+        if (status.is2xxSuccessful()) {
+            return "新增用户成功";
+        } else {
+            return "新增用户失败";
+        }
+    }
+
+    @GetMapping("user/update")
+    public void updateUser() {
+        User user = new User(1L, "mrbird", "123456");
+        this.restTemplate.put("http://SERVER-PROVIDER/user", user);
+    }
+
+    @GetMapping("user/delete/{id:\\d+}")
+    public void deleteUser(@PathVariable Long id) {
+        this.restTemplate.delete("http://SERVER-PROVIDER/user/{1}", id);
+    }
+}
