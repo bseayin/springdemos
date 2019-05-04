0f68b409188f7e66378b17278befde9149d90203
bseayin <yinyouhai@aliyun.com>
2019-05-04 19:42:47 +0800

    bs

---
+++ SpringBootException/src/main/java/com/zz/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.zz.controller;
+
+import com.zz.exception.UserNotExistException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/{id:\\d+}")
+    public void get(@PathVariable String id) {
+
+        throw new UserNotExistException(id);
+    }
+}
